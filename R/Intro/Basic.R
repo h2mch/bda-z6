9071eb490980cb668fe9218b7105e482bdd71bb7
Heinz Marti <hem@zuehlke.com>
2017-11-24 10:38:44 +0100

    work with filter, select, arrange, etc.

--- R/Intro/Basic.R
+++ R/Intro/Basic.R
@@ -24,5 +24,35 @@ ggplot(data = mpg) +
 
 install.packages("nycflights13")
 
-nycflights13::flights
+View(nycflights13::flights)
+
+jan1 <- filter(nycflights13::flights, month == 1, day == 1)
+View(jan1)
+
+novDec <- filter(nycflights13::flights, month %in% c(11, 12))
+
+# Flights with a Delay from more than 2 hours
+twoHours <- 120;
+delayedMoreThan2Hours <- filter(nycflights13::flights, (sched_dep_time + twoHours < dep_time) )
+View(delayedMoreThan2Hours)
+delayedMoreThan2Hours
+
+#Sorting
+arrange(nycflights13::flights, year, month, day)
+
+
+#Selecting some columns
+select(nycflights13::flights, year, month, day)
+
+
+flights_sml <- select(nycflights13::flights, 
+                      year:day, 
+                      ends_with("delay"), 
+                      distance, 
+                      air_time
+)
+View(mutate(nycflights13::flights,
+       gain = arr_delay - dep_delay,
+       speed = distance / air_time * 60
+))
 
